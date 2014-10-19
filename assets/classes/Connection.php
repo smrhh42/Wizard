a6953453e44c0fdbb9f2b38654676c7cca6a8e11
connorTang <azure.connor@gmail.com>
2014-10-19 19:49:58 -0400

    useage + check DB

--- assets/classes/Connection.php
+++ assets/classes/Connection.php
@@ -1,25 +1,35 @@
 <?php
-
+	/*
+	 *	create a database connection, return the connection or null
+	 *	Useage: 
+	 *	require_once('Connection.php');
+	 *	$conn = new Connection();
+	 *	$conn = $conn->ConnectDB();
+	 *	if( $conn !== null ){
+	 *		do query: create,update,delete,read
+	 *	}
+	 *	Altered: Connor, 10.19.2014
+	 */
 	class Connection
 	{
-		
-		
-		
 		private
 		$host		=	"localhost",
-		$db			=	"juanhf_Wizard_Juan",
-		$userName 	=	"juanhf",
-		$password	= 	"Jhf#5479";
-		
+		$db			=	"wizard",
+		$userName 	=	"root",
+		$password	= 	"";
 		
 		public function ConnectDB()
 		{
-					
-			return new PDO("mysql:host=$this->host; dbname=$this->db", $this->userName, $this->password);	
-			print_r($stmt->errorInfo());
-
+			try {		
+				$conn = new PDO("mysql:host=$this->host; dbname=$this->db", $this->userName, $this->password);
+				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			} catch(PDOException $e) {
+				$conn = null;
+				if( $e->getCode() === 2002 ){
+					echo 'Cannot connect to database, please contact your server administrator.';
+				}
+			}
+			return $conn;
 		}
-		
-	}	
-
+	}
 ?>
