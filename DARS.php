963e37a780a3227feb37b1c548cca855934eeac6
Shane Ian Riggins <smrhh42@gmail.com>
2014-11-02 21:36:03 -0500

    Added tabs sections 3 and 4.  Moved a script to the end of the file.

--- DARS.php
+++ DARS.php
@@ -12,24 +12,36 @@ include('header.php');
         
 	  <head>
 	    <meta charset="utf-8">
-	    <title>jQuery UI Tabs - Default functionality</title>
 	    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
 	    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-	    <script src="jquery-ui-1.11.2.js"></script>
+	    <script src="/assets/js/jquery-ui-1.11.2.js"></script>
 	    <link rel="stylesheet" href="/resources/demos/style.css">
 	    <script>
 	    $(function() {
 	      $( "#list1-tabs" ).tabs();
-		  //$( "#list2-tabs").tabs();
 	    });
-	    </script>
-	    <script>
 	    $(function() {
 	      $( "#list2-tabs").tabs();
 	    });
+	    $(function() {
+	      $( "#list3-tabs").tabs();
+	    });
+	    $(function() {
+	      $( "#list4-tabs").tabs();
+	    });
 	    </script>
+		
+		<!--<script type="text/javascript" src = >
+		<script type="text/javascript">
+		function toggleDiv(divId) {
+		   $("#"+divId).toggle();
+		}
+		</script>-->
 	  </head>
 	  <body>
+ <table class="tg">
+	 <tr>
+		 <th class="tg-031e"></th>
  
 	  <div id="list1-tabs">
 	    <ul>
@@ -48,8 +60,11 @@ include('header.php');
 	      <p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
 	    </div>
 	  </div>
+</tr>
 
 
+<tr>
+	<td class="tg-031e"></td>
  <div id="list2-tabs">
    <ul>
      <li><a href="#list-2-tabs-1">Nunc tincidunt</a></li>
@@ -67,6 +82,51 @@ include('header.php');
      <p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
    </div>
  </div>
+</tr>
+ 
+ <tr>
+	 <td class="tg-031e"></td>
+ <div id="list3-tabs">
+   <ul>
+     <li><a href="#list-3-tabs-1">Nunc tincidunt</a></li>
+     <li><a href="#list-3-tabs-2">Proin dolor</a></li>
+     <li><a href="#list-3-tabs-3">Aenean lacinia</a></li>
+   </ul>
+   <div id="list-3-tabs-1">
+     <p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>
+   </div>
+   <div id="list-3-tabs-2">
+     <p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+   </div>
+   <div id="list-3-tabs-3">
+     <p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+     <p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+   </div>
+ </div>
+</tr>
+ 
+ 
+ <tr>
+ <div id="list4-tabs">
+   <ul>
+     <li><a href="#list-4-tabs-1">Nunc tincidunt</a></li>
+     <li><a href="#list-4-tabs-2">Proin dolor</a></li>
+     <li><a href="#list-4-tabs-3">Aenean lacinia</a></li>
+   </ul>
+   <div id="list-4-tabs-1">
+     <p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>
+   </div>
+   <div id="list-4-tabs-2">
+     <p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+   </div>
+   <div id="list-4-tabs-3">
+     <p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+     <p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+   </div>
+ </div>
+ 
+</tr>
+</table>
  
 	  </body>
 	  </html>
@@ -82,8 +142,8 @@ include('header.php');
     <script src="assets/js/bootstrap.min.js"></script>
     <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
     <script src="assets/js/jquery.scrollTo.min.js"></script>
-
-
+	<!--<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+-->
     <!--common script for all pages-->
     <script src="assets/js/common-scripts.js"></script>
 
